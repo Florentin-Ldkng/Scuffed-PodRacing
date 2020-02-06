cba014c4fa56267a3568668b0444101bd0ecf59d
Florentin <florentin.luedeking@t-online.de>
2020-02-06 23:14:10 +0100

    Did Basic Movement and Health
    added first movement and health scripts

---
+++ Scuffed Podracing/Assets/Scripts/Health.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int TotalHealth = 100;
+
+    public void getDamage(int amount)
+    {
+       TotalHealth -= amount;
+    }
+
+}
