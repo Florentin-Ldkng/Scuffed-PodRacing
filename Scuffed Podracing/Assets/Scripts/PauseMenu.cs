28955a391f2510098d22140b6417689e000cf06d
Kevin Keim <kevinkeim@gmx.net>
2020-02-13 19:35:13 +0100

    Fixed Project
    Fixed major bug, added Start Menu and Pause Menu

---
+++ Scuffed Podracing/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    private bool Paused = false;
+
+    public GameObject pauseMenu;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Paused)
+            {
+                Continue();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Continue()
+    {
+        pauseMenu.SetActive(false);
+        Paused = false;
+        Time.timeScale = 1f;
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Paused = true;
+        Time.timeScale = 0f;
+    }
+
+}
