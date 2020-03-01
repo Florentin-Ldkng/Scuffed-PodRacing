2cbab9b2b08d3edc8682f0fc4483c66c3e807323
Kevin Keim <kevinkeim@gmx.net>
2020-03-01 17:37:33 +0100

    Lose Screen
    Created a Lose Screen with functions.

---
+++ Scuffed Podracing/Assets/Scripts/LoseScreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseScreen : MonoBehaviour
+{
+    /// <summary>
+    /// Function for "TryAgainButton" to start the game again
+    /// </summary>
+    public void TryAgain()
+    {
+        Debug.Log("Load Scene Again!");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    /// <summary>
+    /// Function for the "BackToStartMenuButton." Loads the Start Menu
+    /// </summary>
+    public void BackToStartMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
